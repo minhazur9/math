ea0888422cc98d3aa1b36505a61bee9735acb519
minhazur9 <minhazur9@gmail.com>
2020-12-23 21:34:30 -0500

    Added regular polygon calc

--- areas.cpp
+++ areas.cpp
@@ -12,17 +12,36 @@ double calcRectangle(double length, double width) {
 }
 
 double calcTriangle(double base, double height) {
-    double area = (base * height) / 2;
+    double area = base * height / 2;
     return area;
 }
 
-int calcRhombus(int large, int small) {
-    int area = (large * small) / 2;
+double calcRhombus(double large, double small) {
+    int area = large * small / 2;
     return area;
+}
+
+double calcTrapezoid(double large, double small, double height) {
+    int area = ((large + small) / 2) * height;
+    return area;
+}
+
+double calcPolygon(double perimeter, int sides) {
+    double pi = 22.0/7;
+    double sideHalf = perimeter / (sides * 2);
+    double degrees =  (sides - 2) * 180 / (sides * 2);
+    double radians = degrees * (pi / 180);
+    double apothem =  sideHalf * std::tan(radians);
+    double area = perimeter * apothem /2;
+    return area;
+
 }
 
 int main() {
     std::cout << calcSquare(5) << std::endl;
     std::cout << calcRectangle(5,2) << std::endl;
     std::cout << calcTriangle(5,3) << std::endl;
+    std::cout << calcRhombus(5,4) << std::endl;
+    std::cout << calcTrapezoid(10,3,5) << std::endl;
+    std::cout << calcPolygon(30,5) << std::endl;
 }
